fae9b80af698a2ea1e1e9e18dc29afb0b825d47d
宋惟嘉 <feifeige@feifeideMBP.lan>
2022-07-27 14:56:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Personal Portfolio
+
+> This website contains some of my recent projects. If you want to see the live demonstration and source code for each project, click the `See Projects` button for each card, a popup window will appear, scroll down and click either of the `See Live` or `See Source` buttons.
+
+> |Mobile Version|Desktop Version|
+> |--------------|---------------|
+> |![screenshot](Images/Mobile-Version.png)|![screenshot2](Images/Desktop-Version.png)|
+
+## Live Demo Link
+[Live Demo Link](https://gambit142.github.io/Template-Portfolio/)
+
+## Built With
+
+- HTML
+- CSS
+- Visual Studio Code Editor
+- Git & Github
+
+## Getting Started
+To get a local copy up and running:
+1. Clone this repository or download the Zip folder:
+**``https://github.com/Gambit142/Template-Portfolio.git``**
+1. Navigate to the location of the folder in your machine:
+**``you@your-Pc-name:~$ cd <folder>``**
+
+## Authors
+
+👤 **Francis Nzenwa Ugorji**
+
+- GitHub: [@githubhandle](https://github.com/Gambit142)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/francis-ugorji-a567b7168) 
+
+## Contributors
+👤 **Iyadi Cyril**
+
+- GitHub: [@githubhandle](https://github.com/see-why))
+
+👤 **Radmila Stojceva**
+
+- GitHub: [@githubhandle](https://github.com/M0rrighan))
+
+👤 **Ajise Toluwase**
+
+- GitHub: [@githubhandle](https://github.com/Whoistolu))
+
+👤 **Ugbordiegwu Chimaijem Sixtus Paul**
+
+- GitHub: [@githubhandle](https://github.com/Gentilitysp))
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/Gambit142/Template-Portfolio/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to Figma for providing us with their template [Figma Template](https://www.figma.com/file/l7SqJ3ZfkAKih9sFxvWSR4/Microverse-Student-Project-1?node-id=48%3A2346)
+- Hat tip to Microverse for well curated curriculum. Microverse [Home Page](https://bit.ly/MicroverseTN
+)
+- My Coding Partners 🏹
+- My Morning Session Partners ⚡
+- DarkCode for their YouTube tutorials on animation. You can check out their [Instagram Account](https://www.instagram.com/darkcodeonline)
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ main.js
@@ -0,0 +1,143 @@
+// // Create object to store data needed for the this branch feature
+const projects = [
+  {
+    name: 'Connect-With-Mentor React Web App',
+    number: 1,
+    description: `A web app where users can find mentors for career support deployed on Netlify.
+    Implemented log-in/sign-up/mentor database with firebase.
+    Connected to Mailchimp API to send confirmation email Website.
+    `,
+    technologies: ['Firebase', 'ReactJS', 'Redux',  'Styled Component'],
+    image: {
+      imageUrl: './Images/mwm1.jpg',
+      imageAltText: 'Connect-With-Mentor Home Page',
+    },
+    links: ['https://singular-praline-6c1ab9.netlify.app', 'https://github.com/wsong5077/origin.git'],
+  },
+  {
+    name: 'Study Hub React Web App',
+    number: 2,
+    description: `Users can post articles, videos and images and view posts of others.
+    Built with React, Redux, Firebase.`,
+    technologies: ['React', 'Redux', 'Firebase', 'JS'],
+    image: {
+      imageUrl: './Images/sh1.jpg',
+      imageAltText: 'Study Hub',
+    },
+    links: ['https://62e16dcaef9c4d4d61f13158--cosmic-sunshine-669685.netlify.app/', 'https://github.com/wsong5077/study-hub-v2.git'],
+  },
+  
+];
+
+// Create the cards section dynamically using string literals
+function createCard1(project) {
+  const cardString = `<section class="work-container">
+  <article class="cards-container reverse-div${project.number} display-flex">
+    <div class="placeholder">
+    <img class="project-snapshot1" src="${project.image.imageUrl}" alt="${project.image.imageAltText}"/>
+    </div>
+    <div class="details-div display-flex">
+      <div class="cards-header display-flex">
+        <h3 class="style">${project.name}</h3>
+      </div>
+      <div class="card-info">
+        <p class="style">
+        ${project.description}
+        </p>
+      </div>
+      <ul class="languages-div">
+        <li class="lang style">${project.technologies[0]}</li>
+        <li class="lang style">${project.technologies[1]}</li>
+        <li class="lang style">${project.technologies[2]}</li>
+        <li class="ruby lang style">${project.technologies[3]}</li>
+      </ul>
+      <div class="btn-div">
+        <button data-target="modal${project.number}" type="button" class="see-project">
+          See Project
+        </button>
+      </div>
+    </div>
+  </article>
+</section>`;
+  return cardString;
+}
+
+function createModal(project) {
+  const modalInner = `
+    <div class="pop-up-container display-flex" id="modal${project.number}">
+    <img class="closing-button" src="./Images/Closing-Icon.png" alt="Closing Icon">
+      <div class="modal-window modal${project.number}">
+        <div class="title-container display-flex style">
+          <h2>${project.name}</h2>
+        </div>
+        <div class="image-placeholder display-flex">
+          <img class="snapshot1" src="${project.image.imageUrl}" alt="${project.image.imageAltText}">
+          <img class="snapshot2" src= "${project.image.imageUrl}" alt="${project.image.imageAltText}">
+        </div>
+        <div class="card-description display-flex">
+          <p class="card-text style">${project.description}</p>
+        </div>
+        <div class="card-badges display-flex">
+          <h4 class="badge html">${project.technologies[0]}</h4>
+          <h4 class="badge">${project.technologies[1]}</h4>
+          <h4 class="badge">${project.technologies[2]}</h4>
+          <h4 class="badge github">${project.technologies[3]}</h4>
+        </div>
+        <div class="links-div display-flex">
+          <a href= ${project.links[0]} class="live-link">
+            See Live
+            <img class="live-icon" src="./Images/Live-Icon.png" alt="Live Icon">
+          </a>
+          <a href= ${project.links[1]} class="source-link">See Source
+            <img class="github-icon" src="./Images/Github-Logo.png" alt="Github Icon">
+          </a>
+        </div>
+      </div>
+    </div>`;
+  return modalInner;
+}
+
+const projectContainer = document.querySelector('main');
+
+projects.forEach((card) => {
+  projectContainer.innerHTML += createCard1(card);
+});
+
+const modalContainer = document.getElementById('modal-container');
+
+projects.forEach((modal) => {
+  modalContainer.innerHTML += createModal(modal);
+});
+
+// Create functions that opens and close the popup window when see-project button is clicked
+const projectButtons = document.querySelectorAll('.see-project');
+
+projectButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const currentModal = document.getElementById(button.dataset.target);
+    const modalWindows = document.querySelectorAll('.modal-window');
+    currentModal.classList.add('active');
+    modalWindows.forEach((window) => {
+      window.classList.remove('remove-zoom');
+      window.classList.add('add-zoom');
+    });
+    currentModal.style.display = 'flex';
+  });
+});
+
+const closingButtons = document.querySelectorAll('.closing-button');
+
+closingButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const currentModal = document.querySelector('.active');
+    const modalWindows = document.querySelectorAll('.modal-window');
+    modalWindows.forEach((window) => {
+      window.classList.remove('add-zoom');
+      window.classList.add('remove-zoom');
+    });
+    setTimeout(() => {
+      currentModal.classList.remove('active');
+      currentModal.style.display = 'none';
+    }, 1000);
+  });
+});
